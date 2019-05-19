228b31c5b876b937f2b4391fdb83b00e51f19f37
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-05-19 21:34:40 +0200

    Added the Label

---
+++ WGP.NET-AZURUI/Label.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace WGP.AzurUI
+{
+    /// <summary>
+    /// A plain text widget.
+    /// </summary>
+    public class Label : Widget
+    {
+        #region Private Fields
+
+        private Text _display;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="text">Displayed text.</param>
+        public Label(string text = "")
+        {
+            _display = new Text(text, Engine.BaseFont, Engine.CharacterSize);
+            _display.FillColor = Engine.BaseFontColor;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Enabled is useless here, don't use it.
+        /// </summary>
+        [Browsable(false), Bindable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), EditorBrowsable(EditorBrowsableState.Never)]
+        public new bool Enabled { get => base.Enabled; set => base.Enabled = value; }
+
+        /// <summary>
+        /// Hue is useless here, don't use it.
+        /// </summary>
+        [Browsable(false), Bindable(false), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), EditorBrowsable(EditorBrowsableState.Never)]
+        public new float Hue { get => base.Hue; set => base.Hue = value; }
+
+        public override FloatRect LocalBounds => new FloatRect(Position, _display.GetGlobalBounds().Size());
+
+        /// <summary>
+        /// Displayed Text.
+        /// </summary>
+        public virtual string Text { get => _display.DisplayedString; set => _display.DisplayedString = value; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override void DrawOn(RenderTarget target)
+        {
+            Transform tr = Transform.Identity;
+            tr.Translate(Position);
+            target.Draw(_display, new RenderStates(tr));
+        }
+
+        public override void Update(RenderWindow app)
+        {
+        }
+
+        #endregion Public Methods
+    }
+}
